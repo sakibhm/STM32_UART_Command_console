049c442171cb733fb38050b8c6e8926a755720ca
Sakib <hossainsakib9415@gmail.com>
2026-04-06 22:43:27 +0530

    added Help feature

--- main.c
+++ main.c
@@ -6,6 +6,53 @@
 #include "led.h"
 #include "uart.h"
 
+/*typedef struct{
+    char* name;
+    char* desc;
+}command;
+
+command commands[] = {
+    {">led on","-turns on the LED"},
+    {">led off","-turns off the LED"},
+    {">help","-shows list of all commands"}
+};
+
+void print_cmd(void){
+
+    int c = 0;
+
+    while(c<3){
+
+        int n = 0;
+        while(commands[c].name[n] != '\0')
+        {
+            USART1_WaitTxe();
+             USART1_DR = commands[c].name[n];
+             n ++;
+        }
+
+        n = 0;
+
+        while(commands[c].desc[n] != '\0')
+        {
+            USART1_WaitTxe();
+            USART1_DR = commands[c].desc[n];   will fix it later
+            n ++;
+
+        }
+
+        USART1_WaitTxe();
+        USART1_DR = '\r';
+
+        USART1_WaitTxe();
+        USART1_DR = '\n';
+
+
+        c++;
+    }
+}*/ 
+
+
 int main (void)
 {
     AHB1_CLK_ENABLE();  //AHB1 ENABLED
@@ -66,6 +113,61 @@ int main (void)
             else if(strcmp(buffer,"led off") == 0)
             {
                 LED_OFF();
+
+            }
+            else if(strcmp(buffer,"help") == 0)
+            {
+               // print_cmd();   will fix it later
+               char cmd1[] = ">led on - turns on the led";
+                int a = 0;
+                
+                while(cmd1[a] != '\0')
+                {
+                    USART1_WaitTxe();
+                    USART1_DR = cmd1[a];
+                    a ++;
+                }
+                USART1_WaitTxe();
+                USART1_DR = '\r';
+
+                USART1_WaitTxe();
+                USART1_DR = '\n';
+
+                char cmd2[] = ">led off - turns off the led";
+                int b = 0;
+                
+                while(cmd2[b] != '\0')
+                {
+                    USART1_WaitTxe();
+                    USART1_DR = cmd2[b];
+                    b ++;
+                }
+                USART1_WaitTxe();
+                USART1_DR = '\r';
+
+                USART1_WaitTxe();
+                USART1_DR = '\n';
+
+                char cmd3[] = ">help - shows list of commands";
+                int c = 0;
+                
+                while(cmd3[c] != '\0')
+                {
+                    USART1_WaitTxe();
+                    USART1_DR = cmd3[c];
+                    c ++;
+                }
+                USART1_WaitTxe();
+                USART1_DR = '\r';
+
+                USART1_WaitTxe();
+                USART1_DR = '\n';
+
+
+
+
+
+
 
             }
             else
